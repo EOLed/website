eca29110bfb8fbce63c0e52fc10407b4a4a2d082
Amos Chan <amosschan@gmail.com>
2015-03-21 22:09:46 -0400

    Improve database seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 group = Basechurch::Group.create(name: 'English Service')
 user = Basechurch::User.create(email: 'test@example.com', password: 'password')
-bulletin = Basechurch::Bulletin.create(published_at: DateTime.now,
+bulletin = Basechurch::Bulletin.create(published_at: 1.year.ago,
                                        group: group,
                                        name: 'Sunday Service',
                                        description: 'This is a service bulletin.',
-                                       service_order: 'This is the service order.')
+                                       service_order: ' - ## Call to Worship - ## Praise & Worship Edwin\'s Team - ## Announcements - ## Offering - ## Sermon "Our Father" by Pastor Ryan Lee (Matthew 6:9) - ## Doxology - ## Benediction')
 post = Basechurch::Post.create(author: user,
                                group: group,
                                title: 'This is a title',
@@ -12,13 +12,13 @@ post = Basechurch::Post.create(author: user,
                                published_at: DateTime.now)
 Basechurch::Announcement.create(post: post,
                                 bulletin: bulletin,
-                                description: 'This is an announcement',
+                                description: '**Welcome** friends and visitors. We invite you to make MCAC your spiritual home. Please let us know how we can be of help to you.',
                                 position: 1)
 Basechurch::Announcement.create(post: post,
                                 bulletin: bulletin,
-                                description: 'This is the second announcement',
+                                description: 'The 2015 Church theme is **"Heaven Bound."** "Therefore, since we are surrounded by such a great cloud of witnesses, let us throw off everything that hinders and the sin that so easily entangles, and let us run with perseverance the race marked out for us. Let us fix our eyes on Jesus..." (Hebrews 12:1-2)',
                                 position: 2)
 Basechurch::Announcement.create(post: post,
                                 bulletin: bulletin,
-                                description: 'This is the third announcement',
+                                description: 'At last week\'s **Annual Membership Meeting**, we accepted the 2014 Ministry Reports and passed 2015 Financial Budget. The General Fund budget is $518,195 and the Mission Fund is $111,500. Weekly budget is $12,109.52 (an increase of 6.56%). Continue to remember and support church ministries.',
                                 position: 3)
